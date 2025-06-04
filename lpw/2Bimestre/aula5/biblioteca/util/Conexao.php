de7ab0278b57230a07af473a8e53d083515e9a81
Ana Júlia Toledo <ana.toledo.foz@gmail.com>
2025-06-04 19:11:50 -0300

    aaaaaa

--- lpw/2Bimestre/aula5/biblioteca/util/Conexao.php
+++ lpw/2Bimestre/aula5/biblioteca/util/Conexao.php
@@ -19,7 +19,7 @@ class Conexao{
            self::$conn =
                new PDO("mysql:host=localhost:3306;dbname=db_biblioteca",
                        "root",
-                       "bancodeados", $opcoes);
+                       "bancodedados", $opcoes);
         }
        
         return self::$conn;
