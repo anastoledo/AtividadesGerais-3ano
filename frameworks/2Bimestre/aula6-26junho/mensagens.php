71728435050d01f8b26524238236e45f24c3a472
Ana Júlia Toledo <134728792+anastoledo@users.noreply.github.com>
2025-06-26 15:44:01 -0300

    Add files via upload

---
+++ frameworks/2Bimestre/aula6-26junho/mensagens.php
@@ -0,0 +1,4 @@
+<?php
+$mensagens = ['Erro 0: No foi possível criar diretórios ',
+    'Erro 1: Falha ao conectar ao banco de dados',
+    'Aviso: Para fazer o download <a href=sistema.zip>AQUII</a>'];
