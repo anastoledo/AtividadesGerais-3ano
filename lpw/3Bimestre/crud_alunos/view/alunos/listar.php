4d755da36c090de4030be1ad2cf52a85897cc108
Aluno <aluno-lab@ifpr.com>
2025-07-04 15:45:08 -0300

    aula 4 de julho MVC_CRUD

---
+++ lpw/3Bimestre/crud_alunos/view/alunos/listar.php
@@ -0,0 +1,16 @@
+
+<?php 
+
+//INCLUIR O HEADER
+include_once(__DIR__ . "/../include/header.php");
+
+?>
+
+Listagem de Alunos
+
+<?php 
+include_once(__DIR__ . "/../include/footer.php");
+
+//INCLUIR FOOTER
+
+?>
---
+++ lpw/3Bimestre/crud_alunos/view/include/footer.php
@@ -0,0 +1,5 @@
+    
+<p>AAAAA</p>
+
+</body>
+</html>
